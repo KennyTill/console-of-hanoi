6e984d75d2220c4c82719022a95d4e8555fec0fd
Kenneth Till <semisnowy@gmail.com>
2014-06-28 16:57:36 -0700

    adding source files, game board setting up properly

---
+++ consolehanoi/GameBoard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolehanoi
+{
+    /// <summary>
+    /// Holds the game! 
+    /// </summary>
+    class GameBoard
+    {
+        tower towA = new tower();
+        tower towB = new tower();
+        tower towC = new tower();
+        
+        
+        public GameBoard()
+        {
+            //set up the game
+            towA.fill();
+        }
+
+        public void printStatus()
+        {
+            Console.WriteLine(towA.slot1.ToString() + " | " + towB.slot1.ToString() + " | " + towC.slot1.ToString());
+            Console.WriteLine(towA.slot2.ToString() + " | " + towB.slot2.ToString() + " | " + towC.slot2.ToString());
+            Console.WriteLine(towA.slot3.ToString() + " | " + towB.slot3.ToString() + " | " + towC.slot3.ToString());
+            Console.WriteLine(towA.slot4.ToString() + " | " + towB.slot4.ToString() + " | " + towC.slot4.ToString());
+
+        }
+
+
+    }
+}
---
+++ consolehanoi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolehanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameBoard game = new GameBoard();
+            game.printStatus();
+            Console.ReadKey();
+
+        }
+    }
+}
