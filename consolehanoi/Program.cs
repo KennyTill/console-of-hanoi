9aff99b3298700467fa7ae698130f00f69e67df3
Kenneth Till <semisnowy@gmail.com>
2014-06-29 13:24:01 -0700

    Added in instructions in the beginning

--- consolehanoi/Program.cs
+++ consolehanoi/Program.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace consolehanoi
 {
+	/// <summary>
+	/// Created by Kenneth Till 6-28-14
+	/// Free for open source use!
+	/// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             string[] inputs = {"1", "2", "3"};
             GameBoard game = new GameBoard();
+
+			Console.WriteLine("Welcome to Console of Hanoi!");
+			Console.WriteLine("The objective it to get all the disks from tower 1, onto tower 2 or 3.");
+			Console.WriteLine("");
+			Console.WriteLine("You can only move one disk at a time, and cannot place or take disks from under other disks");
+			Console.WriteLine("");
+			Console.WriteLine("Good luck and have fun!");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+			Console.Clear();
+
+
             while (!game.winCondition())
             {
                 game.printStatus();
